ed336d89e74730115d4000004020ae19eee11549
MichalKonradSulek <michal2sulek@gmail.com>
2022-06-18 11:33:27 +0200

    Modyfikacja pixel-speed (zmiany pod raport)

--- main_pixel_speed_copy.py
+++ main_pixel_speed_copy.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
 
     video_provider = Monodepth2VideoInterpreter(video_path)
     speed_calculator = SpeedCalculator(frame_shape=video_provider.frame_shape, n_of_considered_frames=30)
+    speed_calculator_2 = SpeedCalculator(frame_shape=video_provider.frame_shape, n_of_considered_frames=2)
     collision_detector = CollisionDetector(frame_shape=video_provider.frame_shape, collision_time_threshold=5,
                                            n_of_subsequent_frames_required=30)
     timer = MyTimer()
@@ -40,14 +41,19 @@ if __name__ == '__main__':
         speed_negative = pixel_speed.clip(max=0.0)
         speed_to_show = np.dstack((speed_positive, -speed_negative, zeros))
         speed_to_show /= 5.0
-        cv2.imshow("speed", speed_to_show)
+
+        # cv2.imshow("speed", speed_to_show)
         scaled_time = - time_to_collision / 5 + 1
         time_to_show = np.dstack((zeros, zeros, scaled_time))
-        cv2.imshow("time to collision", time_to_show)
+        # cv2.imshow("time to collision", time_to_show)
         scaled_collisions_time = - collisions / 5 + 1
         collisions_to_show = np.dstack((zeros, zeros, scaled_collisions_time))
-        cv2.imshow("collisions", collisions_to_show)
-        cv2.waitKey(1)
+        # cv2.imshow("collisions", collisions_to_show)
+
+        depth_to_show = np.dstack((depth_to_show, depth_to_show, depth_to_show))
+        cv2.imshow("show", np.concatenate((depth_to_show, speed_to_show, time_to_show)))
+
+        cv2.waitKey(0)
         timer.end_period("show")
 
         timer.print_periods()
